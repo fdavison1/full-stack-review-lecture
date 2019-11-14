192a18d1006d37d85197ba19f6cda982585c20ce
fdavison1 <fred.noreply@gmail.com>
2019-11-14 12:31:29 -0700

    lunch break

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,9 +12,6 @@ const Header = () => {
             
             
             
-            Header.jsx
-            
-            
             </header>
     )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,24 +1,59 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-export default class Register extends React.Component{
+export default class Register extends React.Component {
     state = {
         email: '',
         name: '',
         password1: '',
         password2: ''
     }
-    render(){
-        return(
+
+
+    handleChange = (key, value) => {
+        this.setState({
+            [key]: value
+        })
+    }
+
+    render() {
+        return (
             <div>
-                
-                
-                
-                
-                Register
-                
-                
-                
-                
+
+
+
+
+                <input
+                value={this.state.email}
+                    placeholder='email'
+                    onChange={e => this.handleChange('name', e.target.value)} type="text" />
+                <input
+                value={this.state.name}
+                    onChange={e => this.handleChange('name', e.target.value)}
+                    placeholder='name'
+                    type="text" />
+                <input
+                value={this.state.password1}
+                    onChange={e => this.handleChange('password1', e.target.value)}
+                    placeholder='password'
+                    type="password" />
+                <input
+                value={this.state.password2}
+                    onChange={e => this.handleChange('password2', e.target.value)}
+                    placeholder='retype password'
+                    type="password" />
+
+
+
+
+                    <button>register</button>
+
+                    <Link to='/'>
+                    <h4>login</h4>
+                    </Link>
+
+
+
             </div>
         )
     }
